be72f82ede468e395630a90bf74e64c8878a0c29
Shiva Sitamraju <shiva@blockonomics.co>
2016-12-23 16:46:47 +0530

    Removed unsused files. Updated version

--- blockonomics.php
+++ blockonomics.php
@@ -12,7 +12,7 @@ class Blockonomics extends PaymentModule
   {
     $this->name = 'blockonomics';
     $this->tab = 'payments_gateways';
-    $this->version = '1.0.0';
+    $this->version = '1.0.3';
     $this->author = 'Blockonomics';
     $this->need_instance = 1;
     $this->bootstrap = true;
