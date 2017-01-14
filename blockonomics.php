d7662feff1489155459c3ea0aacf3e7c527b85af
Shiva Sitamraju <shiva@blockonomics.co>
2017-01-14 13:43:04 +0530

    Increased version number

--- blockonomics.php
+++ blockonomics.php
@@ -32,7 +32,7 @@ class Blockonomics extends PaymentModule
     {
         $this->name = 'blockonomics';
         $this->tab = 'payments_gateways';
-        $this->version = '1.0.0';
+        $this->version = '1.0.6';
         $this->author = 'Blockonomics';
         $this->need_instance = 1;
         $this->bootstrap = true;
