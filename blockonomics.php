508b4d3322e187dead8c5f25ffdc965e3e323cf1
Juha Siivikko <juha@siivikko.fi>
2018-03-16 13:11:57 +0200

    Version number increase

--- blockonomics.php
+++ blockonomics.php
@@ -32,7 +32,7 @@ class Blockonomics extends PaymentModule
     {
         $this->name = 'blockonomics';
         $this->tab = 'payments_gateways';
-        $this->version = '1.5.2';
+        $this->version = '1.5.3';
         $this->author = 'Blockonomics';
         $this->need_instance = 1;
         $this->bootstrap = true;
