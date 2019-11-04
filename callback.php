efc4a7dfaa7db816f5ab8c7f5c301e8c5ce01155
Freefiona85 <freefiona85@gmail.com>
2019-11-04 13:05:03 +0700

    update kernel declaration (works in old and new prestashop)

--- callback.php
+++ callback.php
@@ -29,7 +29,6 @@ $addr = Tools::getValue('addr');
 //Match secret for callback
 if ($secret == Configuration::get('BLOCKONOMICS_CALLBACK_SECRET')) {
      // Update kernel initialization for Prestashop 1.7.6.1
-    global $kernel;
     if (!$kernel) {
             require_once _PS_ROOT_DIR_.'/app/AppKernel.php';
             $kernel = new \AppKernel('prod', false);
