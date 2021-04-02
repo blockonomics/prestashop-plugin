4c75c5ea885b30da3a6679adb5be53042c0fa8ba
cnohall <christopher.nohall@gmail.com>
2021-04-02 19:45:08 +0900

    defining currency in context if it doesn't exist

--- callback.php
+++ callback.php
@@ -89,6 +89,7 @@ if ($secret == Configuration::get('BLOCKONOMICS_CALLBACK_SECRET')) {
                 if ($order[0]['bits'] > $order[0]['bits_payed']) {
                     $o->setCurrentState(Configuration::get('PS_OS_ERROR'));
                 } else {
+                    Context::getContext()->currency = Context::getContext()->currency ?? new Currency(Configuration::get('PS_CURRENCY_DEFAULT'));
                     $o->setCurrentState(Configuration::get('PS_OS_PAYMENT'));
                 }
             }
