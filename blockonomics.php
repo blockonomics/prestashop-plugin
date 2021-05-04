92d08564a5cad58d315ade46134d40e3394b7526
cnohall <christopher.nohall@gmail.com>
2021-05-04 15:32:06 +0900

    brought up to presta standards

--- blockonomics.php
+++ blockonomics.php
@@ -624,7 +624,7 @@ class Blockonomics extends PaymentModule
         );
         // Init Currencies Fields form array
         $desc = $this->l('To configure, click ') .
-        '<b>'. $this->l('Get Started for Free'). '</b>' . 
+        '<b>'. $this->l('Get Started for Free'). '</b>' .
         $this->l(' on ');
 
         $fields_form[1]['form'] = array(
@@ -635,7 +635,8 @@ class Blockonomics extends PaymentModule
                 array(
                     'type' => 'checkbox',
                     'label'     => $this->l('Bitcoin (BTC)'),
-                    'desc'      => $desc . '<a href="https://blockonomics.co/merchants">https://blockonomics.co/merchants</a>',
+                    'desc'      => $desc . 
+                    '<a href="https://blockonomics.co/merchants">https://blockonomics.co/merchants</a>',
                     'name' => 'BLOCKONOMICS',
                     'values' => array(
                         'query' => array(
@@ -651,7 +652,8 @@ class Blockonomics extends PaymentModule
                 array(
                     'type'      => 'checkbox',
                     'label'     => $this->l('Bitcoin Cash (BCH)'),
-                    'desc'      => $desc . '<a href="https://bch.blockonomics.co/merchants">https://bch.blockonomics.co/merchants</a>',
+                    'desc'      => $desc . 
+                    '<a href="https://bch.blockonomics.co/merchants">https://bch.blockonomics.co/merchants</a>',
                     'name'      => 'BLOCKONOMICS',
                     'values' => array(
                         'query' => array(
