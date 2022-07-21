ac3c1493ea26ce565d86986ce0fca16cd1700a8d
ankit61d <ankit61d@gmail.com>
2022-07-21 10:57:49 +0530

    (fix) PS Module validator warnings

--- translations/de.php
+++ translations/de.php
@@ -2,52 +2,101 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{blockonomics}prestashop>blockonomics_2cd248faec609d132624980e19c7ca97'] = 'Bitcoin - Blockonomics';
-$_MODULE['<{blockonomics}prestashop>blockonomics_c0de713a1b5c4354ebbd1e2227ce971b'] = 'Modul zur Annahmen Bitcoin-Zahlungen';
-$_MODULE['<{blockonomics}prestashop>blockonomics_876f23178c29dc2552c0b48bf23cd9bd'] = 'Modul wirklich deinstallieren?';
-$_MODULE['<{blockonomics}prestashop>blockonomics_2f14ecdf24da27a9fb0ded962aa17e8c'] = 'Bitte spezifizieren Sie einen API-Schlüssel';
-$_MODULE['<{blockonomics}prestashop>blockonomics_575792812af3733426415dc226913037'] = 'Mit Bitcoin bezahlen';
-$_MODULE['<{blockonomics}prestashop>blockonomics_db84ee92e4da32563f91afe070e37a7e'] = 'Ihr Server blockiert ausgehende HTTPS-Calls';
-$_MODULE['<{blockonomics}prestashop>blockonomics_71d736bf8acac6f396861493102750d1'] = 'API-Schlüssel ist nicht korrekt';
-$_MODULE['<{blockonomics}prestashop>blockonomics_baa8441e3dbb790354e36e17e77d960a'] = 'Bitte fügen Sie einen neuen Shop auf der blockonomics-website hinzu';
-$_MODULE['<{blockonomics}prestashop>blockonomics_98c2637920a4f98d40f7f6b0fc2fd378'] = 'Ausführlichere Information finden Sie in diesen';
-$_MODULE['<{blockonomics}prestashop>blockonomics_c3d515262545a5bde7c683f68afe06f9'] = 'Artikel zur Fehlerbehebung';
-$_MODULE['<{blockonomics}prestashop>blockonomics_f75cbf4937d2f1f31e6a4c727ebfee7b'] = 'Setup wurde abgeschlossen!';
-$_MODULE['<{blockonomics}prestashop>blockonomics_e576911d3083c76abba01c8a03842856'] = 'Die Einstellungen wurden gespeichert, klicken Sie auf TestSetup, um die Installation zu überprüfen';
-$_MODULE['<{blockonomics}prestashop>blockonomics_f4f70727dc34561dfde1a3c529b6205c'] = 'Einstellungen';
-$_MODULE['<{blockonomics}prestashop>blockonomics_d876ff8da67c3731ae25d8335a4168b4'] = 'API-Schlüssel';
-$_MODULE['<{blockonomics}prestashop>blockonomics_4090c73f435e12febebe689cdf79031e'] = 'URL HTTP-CALLBACK ';
-$_MODULE['<{blockonomics}prestashop>blockonomics_ff64c6d108f9d4760aaf521fbbabbe8f'] = 'Dauer';
-$_MODULE['<{blockonomics}prestashop>blockonomics_1d916369325e045bba284bcbce35ccaf'] = 'Countdown-Timer auf Bezahlseite';
-$_MODULE['<{blockonomics}prestashop>blockonomics_70abb32fcb0c9e8194526d1eef15eb05'] = '10 Minuten';
-$_MODULE['<{blockonomics}prestashop>blockonomics_2a7bb67d9681d8b5c8241cd49c734772'] = '15 Minuten';
-$_MODULE['<{blockonomics}prestashop>blockonomics_f31f3ebbdee89a50923676a58da95904'] = '20 Minuten';
-$_MODULE['<{blockonomics}prestashop>blockonomics_92f08914683ec10736d27ad9a15fa624'] = '25 Minuten';
-$_MODULE['<{blockonomics}prestashop>blockonomics_0d714869027c4e08ea9b2943d9bd704e'] = '30 Minuten';
-$_MODULE['<{blockonomics}prestashop>blockonomics_c9cc8cce247e49bae79f15173ce97354'] = 'Speichern';
-$_MODULE['<{blockonomics}prestashop>blockonomics_a17379a9752f2e6eef6c319c771f5f44'] = 'Zum Konfigurieren die Schaltfläche, anklicken';
-$_MODULE['<{blockonomics}prestashop>blockonomics_64f676ae6e961fa1e596eeeeef5c7df9'] = 'Get Started for Free';
-$_MODULE['<{blockonomics}prestashop>blockonomics_ed2b5c0139cec8ad2873829dc1117d50'] = 'auf';
-$_MODULE['<{blockonomics}prestashop>blockonomics_dfcfc43722eef1eab1e4a12e50a068b1'] = 'Währungen';
-$_MODULE['<{blockonomics}prestashop>blockonomics_bfacb577033fa77ff21438e5ff80ca3b'] = 'Bitcoin (BTC)';
-$_MODULE['<{blockonomics}prestashop>blockonomics_9cb7ca5565d6c28ff385f66a173c86a7'] = 'Bitcoin Cash (BCH)';
-$_MODULE['<{blockonomics}prestashop>blockonomics_b279be2ec798cea1e5019824662e53e9'] = 'Test Setup';
-$_MODULE['<{blockonomics}prestashop>payment_d1228f5476d15142b1358ae4b5fa2454'] = 'Bestell-Nr';
-$_MODULE['<{blockonomics}prestashop>payment_8fd2d4cb88dd67be9d343d3398ca763d'] = 'Zahlung abgelaufen';
-$_MODULE['<{blockonomics}prestashop>payment_79d7ff78f59371847aa73f232be2d196'] = 'Hier klicken, um es erneut zu versuchen';
-$_MODULE['<{blockonomics}prestashop>payment_9e553ea6221b688a01e329cf76dde4bf'] = 'Der bezahlte BTC-Betrag der Bestellung ist niedriger als erwartet. Händler kontaktieren';
-$_MODULE['<{blockonomics}prestashop>payment_a217a4286be5ab0bd03a98d33a78c420'] = 'In Wallet öffnen';
-$_MODULE['<{blockonomics}prestashop>payment_4972448661ca72aa783da607f9b3ecc7'] = 'Zum Bezahlen genau diesen Betrag senden';
-$_MODULE['<{blockonomics}prestashop>payment_efc1a46f7b47e4d3cf60f13b6c89069b'] = 'In Zwischenablage kopiert';
-$_MODULE['<{blockonomics}prestashop>payment_b187de06406775bc913d0c12c6a5dbe6'] = 'An diese';
-$_MODULE['<{blockonomics}prestashop>payment_ea08ae01e95bb85ba4d51f7fa224a956'] = ' Adresse';
-$_MODULE['<{blockonomics}prestashop>payment_df9e1aa4940f7a34263d65ef41b87cf1'] = 'min';
-$_MODULE['<{blockonomics}prestashop>payment_7f0801830c4f3ed63e81fc0053177394'] = 'Wie kann ich bezahlen? | Bewertungen zu diesem Shop anzeigen';
-$_MODULE['<{blockonomics}prestashop>payment_69616e342259a7da05c26802caf4f0ea'] = 'Bereitgestellt von Blockonomics';
-$_MODULE['<{blockonomics}prestashop>no_crypto_7a686f1321744105bd4183854107d431'] = 'Für den Zahlvorgang sind keine Kryptowährungen aktiviert';
-$_MODULE['<{blockonomics}prestashop>no_crypto_6387ce01d8a5c7eff4bfc2e74afc1767'] = 'Hinweis an den Webmaster: Aktivierung über PrestaShop Admin > Module > Modul-Manager > Bitcoin - Blockonomics > Währungen';
-$_MODULE['<{blockonomics}prestashop>select_03a7faed722f23ed1f61d3a49425200b'] = 'Bezahlen mit';
-$_MODULE['<{blockonomics}prestashop>validation_02cb6e7914265feb537fe7078c7a5649'] = 'PHP-Erweiterung fehlt.';
-$_MODULE['<{blockonomics}prestashop>validation_5dbacc8b4262eb20aa2ec42ab2a1ac0b'] = 'Bitte die fehlende php-intl-Erweiterung installieren';
-$_MODULE['<{blockonomics}prestashop>payment_eb60ab14749e5732652e66fbaf2bd75e'] = 'Bitcoin-Adresse kann nicht generiert werden.';
-$_MODULE['<{blockonomics}prestashop>payment_b5511b23e304849b337641e03b9223de'] = 'Bitte verwenden Sie die TestSetup-Schaltfläche in der Konfiguration, um den Fehler zu diagnostizieren';
+$_MODULE['<{blockonomics}prestashop>blockonomics_2cd248faec609d132624980e19c7ca97']
+    = 'Bitcoin - Blockonomics';
+$_MODULE['<{blockonomics}prestashop>blockonomics_c0de713a1b5c4354ebbd1e2227ce971b']
+    = 'Modul zur Annahmen Bitcoin-Zahlungen';
+$_MODULE['<{blockonomics}prestashop>blockonomics_876f23178c29dc2552c0b48bf23cd9bd']
+    = 'Modul wirklich deinstallieren?';
+$_MODULE['<{blockonomics}prestashop>blockonomics_2f14ecdf24da27a9fb0ded962aa17e8c']
+    = 'Bitte spezifizieren Sie einen API-Schlüssel';
+$_MODULE['<{blockonomics}prestashop>blockonomics_575792812af3733426415dc226913037']
+    = 'Mit Bitcoin bezahlen';
+$_MODULE['<{blockonomics}prestashop>blockonomics_db84ee92e4da32563f91afe070e37a7e']
+    = 'Ihr Server blockiert ausgehende HTTPS-Calls';
+$_MODULE['<{blockonomics}prestashop>blockonomics_71d736bf8acac6f396861493102750d1']
+    = 'API-Schlüssel ist nicht korrekt';
+$_MODULE['<{blockonomics}prestashop>blockonomics_baa8441e3dbb790354e36e17e77d960a']
+    = 'Bitte fügen Sie einen neuen Shop auf der blockonomics-website hinzu';
+$_MODULE['<{blockonomics}prestashop>blockonomics_98c2637920a4f98d40f7f6b0fc2fd378']
+    = 'Ausführlichere Information finden Sie in diesen';
+$_MODULE['<{blockonomics}prestashop>blockonomics_c3d515262545a5bde7c683f68afe06f9']
+    = 'Artikel zur Fehlerbehebung';
+$_MODULE['<{blockonomics}prestashop>blockonomics_f75cbf4937d2f1f31e6a4c727ebfee7b']
+    = 'Setup wurde abgeschlossen!';
+$_MODULE['<{blockonomics}prestashop>blockonomics_e576911d3083c76abba01c8a03842856']
+    = 'Die Einstellungen wurden gespeichert, klicken Sie auf TestSetup, um die Installation zu überprüfen';
+$_MODULE['<{blockonomics}prestashop>blockonomics_f4f70727dc34561dfde1a3c529b6205c']
+    = 'Einstellungen';
+$_MODULE['<{blockonomics}prestashop>blockonomics_d876ff8da67c3731ae25d8335a4168b4']
+    = 'API-Schlüssel';
+$_MODULE['<{blockonomics}prestashop>blockonomics_4090c73f435e12febebe689cdf79031e']
+    = 'URL HTTP-CALLBACK ';
+$_MODULE['<{blockonomics}prestashop>blockonomics_ff64c6d108f9d4760aaf521fbbabbe8f']
+    = 'Dauer';
+$_MODULE['<{blockonomics}prestashop>blockonomics_1d916369325e045bba284bcbce35ccaf']
+    = 'Countdown-Timer auf Bezahlseite';
+$_MODULE['<{blockonomics}prestashop>blockonomics_70abb32fcb0c9e8194526d1eef15eb05']
+    = '10 Minuten';
+$_MODULE['<{blockonomics}prestashop>blockonomics_2a7bb67d9681d8b5c8241cd49c734772']
+    = '15 Minuten';
+$_MODULE['<{blockonomics}prestashop>blockonomics_f31f3ebbdee89a50923676a58da95904']
+    = '20 Minuten';
+$_MODULE['<{blockonomics}prestashop>blockonomics_92f08914683ec10736d27ad9a15fa624']
+    = '25 Minuten';
+$_MODULE['<{blockonomics}prestashop>blockonomics_0d714869027c4e08ea9b2943d9bd704e']
+    = '30 Minuten';
+$_MODULE['<{blockonomics}prestashop>blockonomics_c9cc8cce247e49bae79f15173ce97354']
+    = 'Speichern';
+$_MODULE['<{blockonomics}prestashop>blockonomics_a17379a9752f2e6eef6c319c771f5f44']
+    = 'Zum Konfigurieren die Schaltfläche, anklicken';
+$_MODULE['<{blockonomics}prestashop>blockonomics_64f676ae6e961fa1e596eeeeef5c7df9']
+    = 'Get Started for Free';
+$_MODULE['<{blockonomics}prestashop>blockonomics_ed2b5c0139cec8ad2873829dc1117d50']
+    = 'auf';
+$_MODULE['<{blockonomics}prestashop>blockonomics_dfcfc43722eef1eab1e4a12e50a068b1']
+    = 'Währungen';
+$_MODULE['<{blockonomics}prestashop>blockonomics_bfacb577033fa77ff21438e5ff80ca3b']
+    = 'Bitcoin (BTC)';
+$_MODULE['<{blockonomics}prestashop>blockonomics_9cb7ca5565d6c28ff385f66a173c86a7']
+    = 'Bitcoin Cash (BCH)';
+$_MODULE['<{blockonomics}prestashop>blockonomics_b279be2ec798cea1e5019824662e53e9']
+    = 'Test Setup';
+$_MODULE['<{blockonomics}prestashop>payment_d1228f5476d15142b1358ae4b5fa2454']
+    = 'Bestell-Nr';
+$_MODULE['<{blockonomics}prestashop>payment_8fd2d4cb88dd67be9d343d3398ca763d']
+    = 'Zahlung abgelaufen';
+$_MODULE['<{blockonomics}prestashop>payment_79d7ff78f59371847aa73f232be2d196']
+    = 'Hier klicken, um es erneut zu versuchen';
+$_MODULE['<{blockonomics}prestashop>payment_9e553ea6221b688a01e329cf76dde4bf']
+    = 'Der bezahlte BTC-Betrag der Bestellung ist niedriger als erwartet. Händler kontaktieren';
+$_MODULE['<{blockonomics}prestashop>payment_a217a4286be5ab0bd03a98d33a78c420']
+    = 'In Wallet öffnen';
+$_MODULE['<{blockonomics}prestashop>payment_4972448661ca72aa783da607f9b3ecc7']
+    = 'Zum Bezahlen genau diesen Betrag senden';
+$_MODULE['<{blockonomics}prestashop>payment_efc1a46f7b47e4d3cf60f13b6c89069b']
+    = 'In Zwischenablage kopiert';
+$_MODULE['<{blockonomics}prestashop>payment_b187de06406775bc913d0c12c6a5dbe6']
+    = 'An diese';
+$_MODULE['<{blockonomics}prestashop>payment_ea08ae01e95bb85ba4d51f7fa224a956']
+    = ' Adresse';
+$_MODULE['<{blockonomics}prestashop>payment_df9e1aa4940f7a34263d65ef41b87cf1']
+    = 'min';
+$_MODULE['<{blockonomics}prestashop>payment_7f0801830c4f3ed63e81fc0053177394']
+    = 'Wie kann ich bezahlen? | Bewertungen zu diesem Shop anzeigen';
+$_MODULE['<{blockonomics}prestashop>payment_69616e342259a7da05c26802caf4f0ea']
+    = 'Bereitgestellt von Blockonomics';
+$_MODULE['<{blockonomics}prestashop>no_crypto_7a686f1321744105bd4183854107d431']
+    = 'Für den Zahlvorgang sind keine Kryptowährungen aktiviert';
+$_MODULE['<{blockonomics}prestashop>no_crypto_6387ce01d8a5c7eff4bfc2e74afc1767']
+    = 'Hinweis an den Webmaster: Aktivierung über PrestaShop Admin > Module > Modul-Manager > Bitcoin - Blockonomics > Währungen';
+$_MODULE['<{blockonomics}prestashop>select_03a7faed722f23ed1f61d3a49425200b']
+    = 'Bezahlen mit';
+$_MODULE['<{blockonomics}prestashop>validation_02cb6e7914265feb537fe7078c7a5649']
+    = 'PHP-Erweiterung fehlt.';
+$_MODULE['<{blockonomics}prestashop>validation_5dbacc8b4262eb20aa2ec42ab2a1ac0b']
+    = 'Bitte die fehlende php-intl-Erweiterung installieren';
+$_MODULE['<{blockonomics}prestashop>payment_eb60ab14749e5732652e66fbaf2bd75e']
+    = 'Bitcoin-Adresse kann nicht generiert werden.';
+$_MODULE['<{blockonomics}prestashop>payment_b5511b23e304849b337641e03b9223de']
+    = 'Bitte verwenden Sie die TestSetup-Schaltfläche in der Konfiguration, um den Fehler zu diagnostizieren';
