27904b0ae462a9003ed0c531eb96f7fa2491892b
Darren <darrenwestwood86@gmail.com>
2023-04-24 16:51:29 +0200

    Display type and value of amount

--- callback.php
+++ callback.php
@@ -125,6 +125,7 @@ function insertTXIDIntoPaymentDetails($presta_order, $txid, $blockonomics_order)
         //Payment created, but the txid still hasn't been recorded
         $payments[0]->transaction_id = $txid;
         $payments[0]->payment_method = "Blockonomics - " . Tools::strtoupper($blockonomics_order['crypto']);
+        var_dump($payments[0]->amount);
         $payments[0]->save();
     }
 }
